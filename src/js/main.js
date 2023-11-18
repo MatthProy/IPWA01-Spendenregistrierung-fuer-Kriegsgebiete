63720b3a1e2d6fac46350d3f93610aae98ff377b
Matthias Proy <m.proy@gmx.de>
2023-11-18 12:21:48 +0100

    directderlivery checkbox functionality,disables textfields added

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import * as bootstrap from 'bootstrap'
 //Import the picture for our Logo from our assets
 //source: https://www.iconfinder.com/search?q=clothing&price=free
 import Icon  from '../assets/5548711_apparel_clothes_clothing_fashion_shirt_icon.png'
+import {isDisabled} from "bootstrap/js/src/util";
 
 // Adding the image for the logo
 let logoField = document.getElementById("logoField");
@@ -17,4 +18,19 @@ image.height = 54;
 logoField.appendChild(image);
 
 //direct delivery selected functionality
+let directDelivery = document.getElementById("directDelivery")
+directDelivery.addEventListener("click",(evt) => {
+    let adressField = document.getElementById("adressField");
+    let postNumberField = document.getElementById("postNumberField");
+    if(directDelivery.checked === true){
+        adressField.setAttribute("disabled", "");
+        postNumberField.setAttribute("disabled", "");
+    }else{
+        adressField.removeAttribute("disabled")
+        postNumberField.removeAttribute("disabled");
+    }
+})
+
+
+
 
